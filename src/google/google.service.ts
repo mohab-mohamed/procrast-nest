8e594989f8a027aaccbcfc5759aaf550ae85df7d
Mohab Mohamed <mohab.mohamed@savvas.com>
2021-09-06 00:30:20 -0400

    add logic for sending tasklist data to client

--- src/google/google.service.ts
+++ src/google/google.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
 import { GoogleResponse } from './googleResponse.interface';
-import { Observable } from 'rxjs';
+import { lastValueFrom, Observable } from 'rxjs';
 
 
 @Injectable()
@@ -14,13 +14,27 @@ export class GoogleService {
         const headersRequest = {
             'Authorization': `Bearer ${token}`,
         };
-        console.log(headersRequest);
+        // console.log(headersRequest);
         const params = new URLSearchParams([['timeMin', timeMin], ['timeMax', timeMax]]);
-        console.log(params);
-        return this.httpService.get('https://www.googleapis.com/calendar/v3/calendars/primary/events', { params: params, headers: headersRequest }).toPromise();
+        // console.log(params);
+        return lastValueFrom(this.httpService.get('https://www.googleapis.com/calendar/v3/calendars/primary/events', { params: params, headers: headersRequest }));
     }
 
+    getTaskLists(token: string): Promise<any> {
+        const headersRequest = {
+            'Authorization': `Bearer ${token}`
+        };
+        const params = new URLSearchParams([]);
+        return lastValueFrom(this.httpService.get('https://tasks.googleapis.com/tasks/v1/users/@me/lists', { params: params, headers: headersRequest }));
+    }
 
+    getTasks(token: string, taskList: string): Promise<any> {
+        const headersRequest = {
+            'Authorization': `Bearer ${token}`
+        };
+        const params = new URLSearchParams([]);
+        return lastValueFrom(this.httpService.get(`https://tasks.googleapis.com/tasks/v1/lists/${taskList}/tasks`, { params: params, headers: headersRequest }));
+    }
 
     googleLogin(req: GoogleResponse) {
         if (!req.user) {
